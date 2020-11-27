83d3a5a304df7f130c2bb81e3e3864ce47990b1a
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2020-11-27 15:57:44 +0100

    Minor code cleanin

--- scsbaf_arduino/scsbaf_arduino.ino
+++ scsbaf_arduino/scsbaf_arduino.ino
@@ -24,12 +24,12 @@
 #define K_RG    (uint8_t)215
 
 //page up/down
-#define K_PGUP    (uint8_t)211
-#define K_PGDN    (uint8_t)214
+#define K_PGUP  (uint8_t)211
+#define K_PGDN  (uint8_t)214
 
 //home/end
-#define K_HOME    (uint8_t)210
-#define K_END    (uint8_t)213
+#define K_HOME  (uint8_t)210
+#define K_END   (uint8_t)213
 
 //mod key
 #define K_MOD1 (uint8_t)1
@@ -44,30 +44,35 @@ uint8_t key_matrix[2][40] = {
   K_SHFT, 'z',    'x',    'c',    'v',    'b',    'k',    'm',    ',',    '.', 
   K_CTRL, K_SUPR, K_ALT,  '[',    ']',    ' ',    K_MOD1,                            '/',    'o',    ';'},
                           
-{ '`',    '1',    '2',    '3',    '=',    'g',    '\'',    K_HOME,  K_PGDN,  K_PGUP,
+{ '`',    '1',    '2',    '3',    '=',    'g',    '\'',   K_HOME,  K_PGDN,  K_PGUP,
   K_ESC,  '4',    '5',    '6',    '-',    'd',    K_DEL,  K_LF,    K_DN,    K_UP,
   K_SHFT, '7',    '8',    '9',    '0',    'b',    'k',    'm',     ',',     '.', 
   K_CTRL, K_SUPR, K_ALT,  '[',    ']',    K_RET,  K_MOD1,                            '\\',    K_RG,    K_END},
 
 };
 
-uint8_t type_matrix[2][40] = {
-{ 1,    0,      0,        0,      0,      0,       0,      0,      0,       0,
-  0,    0,      0,        0,      0,      0,       0,      0,      0,       0,
-  1,    0,      0,        0,      0,      0,       0,      0,      0,       0,
-  1,    1,      1,        0,      0,      0,       2,                             0,      0,       0},
+typedef enum {DFLT = 0, HOLD = 1, MCRO = 2}__attribute__((packed)) key_type;
 
-{ 1,    0,      0,        0,      0,      0,       0,      0,      0,       0,
-  0,    0,      0,        0,      0,      0,       0,      0,      0,       0,
-  1,    0,      0,        0,      0,      0,       0,      0,      0,       0,
-  1,    1,      1,        0,      0,      0,       2,                             0,      0,       0},
-};
 //type of key
-//0 - normal keys
-//1 - special, often hold down keys like control, shift, etc
-//2 - macro keys (they change layout if pressed)
+//DFLT - (default) normal keys
+//HOLD - special, often hold down keys like control, shift, etc
+//MCRO - macro keys (they change layout if pressed)
 
-uint8_t state[40] = {0};            //0 - non active, 1 - pushed, 2 - active
+key_type type_matrix[2][40] = {
+{ HOLD,    DFLT,      DFLT,        DFLT,      DFLT,      DFLT,       DFLT,      DFLT,      DFLT,       DFLT,
+  DFLT,    DFLT,      DFLT,        DFLT,      DFLT,      DFLT,       DFLT,      DFLT,      DFLT,       DFLT,
+  HOLD,    DFLT,      DFLT,        DFLT,      DFLT,      DFLT,       DFLT,      DFLT,      DFLT,       DFLT,
+  HOLD,    HOLD,      HOLD,        DFLT,      DFLT,      DFLT,       MCRO,                             DFLT,      DFLT,       DFLT},
+
+{ HOLD,    DFLT,      DFLT,        DFLT,      DFLT,      DFLT,       DFLT,      DFLT,      DFLT,       DFLT,
+  DFLT,    DFLT,      DFLT,        DFLT,      DFLT,      DFLT,       DFLT,      DFLT,      DFLT,       DFLT,
+  HOLD,    DFLT,      DFLT,        DFLT,      DFLT,      DFLT,       DFLT,      DFLT,      DFLT,       DFLT,
+  HOLD,    HOLD,      HOLD,        DFLT,      DFLT,      DFLT,       MCRO,                             DFLT,      DFLT,       DFLT}
+};
+
+typedef enum {INACTIVE = 0, ACTIVE = 1, LONG_ACTIVE = 2}__attribute__((packed)) key_state;
+
+key_state state[40] = {INACTIVE};            //0 - inactive, 1 - pushed, 2 - long_active (if button is pressed down for longer than REPEAT_MS)
 unsigned long saturation[40] = {0}; //time at the moment when the button is pressed
 
 uint8_t pressed;                    //state flag in key_data_upate function
@@ -82,36 +87,36 @@ void key_data_update(uint8_t idx, uint8_t V_state)
     {
       if(state[idx] == 0)
       {
-        state[idx] = 1;
+        state[idx] = ACTIVE;
         pressed = 1;
       }
       else if (state[idx] == 1 && (millis() - saturation[idx] > REPEAT_MS))
       {
+          state[idx] = LONG_ACTIVE;
           pressed = 1;
-          state[idx] = 2;
       }
-      else if (state[idx]== 2){pressed = 1;}
+      else if (state[idx]== LONG_ACTIVE){pressed = 1;}
     }
   }
   else
   {
-    if(type_matrix[modifier][idx] == 1 && state[idx]) Keyboard.release(key_matrix[modifier][idx]);
-    if(type_matrix[modifier][idx] == 2 && state[idx]) modifier = 0;
-    state[idx] = 0;
+    if(type_matrix[modifier][idx] == HOLD && state[idx]) Keyboard.release(key_matrix[modifier][idx]);
+    if(type_matrix[modifier][idx] == MCRO && state[idx]) modifier = 0;
+    state[idx] = INACTIVE;
     saturation[idx] = 0;
   }
   
   if(pressed)
   {
     saturation[idx] = millis();
-    if(type_matrix[modifier][idx] == 0)
+    if(type_matrix[modifier][idx] == DFLT)
     {
       Keyboard.press(key_matrix[modifier][idx]);
       delay(RELAY_TIME);
       Keyboard.release(key_matrix[modifier][idx]);
     }
-    if(type_matrix[modifier][idx] == 1) { Keyboard.press(key_matrix[modifier][idx]); }
-    if(type_matrix[modifier][idx] == 2) {modifier = key_matrix[modifier][idx];}
+    if(type_matrix[modifier][idx] == HOLD) { Keyboard.press(key_matrix[modifier][idx]); }
+    if(type_matrix[modifier][idx] == MCRO) {modifier = key_matrix[modifier][idx];}
   }
 }
 
